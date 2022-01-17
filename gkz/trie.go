6edba4c8005a4f1a94c5d92ec1310b7c626686d7
zhy <2257077749@qq.com>
2022-01-17 14:58:42 +0800

    Day3

--- gkz/trie.go
+++ gkz/trie.go
@@ -1,14 +1,20 @@
 package gkz
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 type node struct {
 	pattern  string  //待匹配路由，例如/p/:lang
 	part     string  // 路由中的一部分，例如：lang
 	children []*node // 子节点，例如【doc,tutorial,intro】
-	iswild   bool    //是否精确匹配，part含有 : 或*时为true
+	isWild   bool    //是否精确匹配，part含有 : 或*时为true
 }
 
+func (n *node) String() string {
+	return fmt.Sprintf("node{pattern=%s, part=%s, isWild=%t}", n.pattern, n.part, n.isWild)
+}
 func (n *node) matchChild(part string) *node {
 	for _, child := range n.children {
 		if child.part == part || child.iswild {
